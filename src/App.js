5da1593e560c4582bbf152fdbcaa23c3e6ed31ea
baklavacado <benny.l.wu@gmail.com>
2020-06-11 19:35:22 -0700

    trim public assets and add background-img

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-// App.js
 import React, { useState, useEffect } from 'react';
 import './App.css';
-
+import Home from './Home';
 import ClimbsTable from './components/ClimbsTable';
 
 export default App => {
@@ -9,26 +8,27 @@ export default App => {
 
   useEffect(() => {
 
-    // async function getClimbs() {
-    //   const res = await fetch('/.netlify/functions/climbRead')
-    //   const data = await res.json()
-    //   console.log(data.data)
-    //   setProblems(data.data)
+    async function getClimbs() {
+      const res = await fetch('/.netlify/functions/getAllProblems')
+      const data = await res.json()
+      console.log(data.data)
+      setProblems(data.data)
 
-    // }
-    fetch('/.netlify/functions/getAllProblems')
-      .then(res => res.json())
-      .then(res => {
-        setProblems(res.data)
-      })
-      .catch(err => console.log('Error retrieving climbs: ', err))
-    // getClimbs()
+    }
+    // fetch('/.netlify/functions/getAllProblems')
+    //   .then(res => res.json())
+    //   .then(res => {
+    //     console.log(res.data[0])
+    //     setProblems(res.data)
+    //   })
+    //   .catch(err => console.log('Error retrieving climbs: ', err))
+    getClimbs()
   }, []);
 
   return (
     <div>
-      {/* <ClimbsTable problems={problems}/> */}
-      {problems.length > 0 ? <ClimbsTable problems={problems}/> : console.log(problems)}
+      {/* {problems.length > 0 ? <ClimbsTable problems={problems}/> : console.log(problems)} */}
+      <Home />
     </div>
   )
 }
---
+++ src/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default Home => {
+  return (
+    // all
+    <>
+    <div>Navbar</div>
+    {/* // no user */}
+    <div>Login</div>
+    <div>Sign Up</div>
+    {/* // user */}
+    <div> User Dashboard</div>
+    </>
+  )
+} 
