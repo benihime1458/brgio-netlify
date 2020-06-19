83a727907f5e064526f84a9976b67dabfcc72305
baklavacado <benny.l.wu@gmail.com>
2020-06-19 15:45:57 -0700

    convert auth ui to react-hook-form

--- src/users/AuthUI.js
+++ src/users/AuthUI.js
@@ -1,31 +1,37 @@
 import React, {useState} from 'react'
+import {useForm} from 'react-hook-form'
 import {Paper, TextField, Button, Tabs, Tab} from '@material-ui/core'
 import './AuthUI.css'
 import fire from './Fire'
 
 export default AuthUI => {
+  const {register, errors, getValues, handleSubmit} = useForm(
+    {
+      mode: 'onChange',
+      reValidateMode: 'onChange',
+    }
+  )
   const [index, setIndex] = useState(0)
-  const [input, setInput] = useState({email: '', username: '', password: '', confirmPassword: ''})
 
-  const login = e => {
-    e.preventDefault()
-    fire.auth().signInWithEmailAndPassword(input.email, input.password).then(u => console.log(u)).catch(error => console.log(error))
-  }
+  const onSub = data => console.log(data)
 
-  const signup = async e => {
-    e.preventDefault()
+  // const login = e => {
+  //   e.preventDefault()
+  //   fire.auth().signInWithEmailAndPassword(input.email, input.password).then(u => console.log(u)).catch(error => console.log(error))
+  // }
 
-    const res = await fetch('/.netlify/functions/getAllProblems')
-    const problems = await res.json()
-    console.log('problems: ', problems)
+  // const signup = async e => {
+  //   e.preventDefault()
+
+  //   const res = await fetch('/.netlify/functions/getAllProblems')
+  //   const problems = await res.json()
+  //   console.log('problems: ', problems)
     
-    let newUser = {username: input.username, email: input.email, problemLog: problems}
+  //   let newUser = {username: input.username, email: input.email, problemLog: problems}
 
-
-
-    fire.auth().createUserWithEmailAndPassword(input.email, input.password).then(u => console.log(u)).catch(error => console.log(error.message))
-  }
-
+  //   fire.auth().createUserWithEmailAndPassword(input.email, input.password).then(u => console.log(u)).catch(error => console.log(error.message))
+  // }
+  
   return (
     <Paper className='paper'>
       <Tabs
@@ -45,47 +51,56 @@ export default AuthUI => {
           onClick={() => setIndex(1)}
         />
       </Tabs>
-      <form onSubmit={index === 0 ? login : signup}>
+      <form onSubmit={handleSubmit(onSub)}>
 
         {
           index === 1 ? <TextField 
           className='formfield'
           label='Desired Username'
-          type='string'
-          value={input.username}
-          onChange={(e) => setInput({...input, username: e.target.value})}
+          type='text'
+          inputRef={register({required: true, minLength: {value:6, message: 'Must be at least 6 characters'}, maxLength: {value: 20, message: 'Cannot be longer than 20 characters'}, pattern: { value: /^[a-zA-Z0-9]*$/i, message: 'Can only contain alphanumeric characters'}})}
+          name='username'
+          error={!!errors.username}
           variant='filled'
+          helperText={errors.username && errors.username.message}
         /> : null
         }
 
         <TextField 
-        className='formfield'
+          className='formfield'
           label='Email'
           type='email'
-          value={input.email}
-          onChange={(e) => setInput({...input, email: e.target.value})}
+          inputRef={register({required: true, pattern: {value: /^\S+@\S+$/i, message: 'Invalid email format'}})}
+          name='email'
+          error={!!errors.email}
           variant='filled'
+          helperText={errors.email && errors.email.message}
         />
         <TextField 
-        className='formfield'
+          className='formfield'
           label='Password'
           type='password'
-          value={input.password}
-          onChange={(e) => setInput({...input, password: e.target.value})}
+          inputRef={register({required: true, minLength: {value:6, message: 'Must be at least 6 characters'}})}
+          name='password'
+          error={!!errors.password}
           variant='filled'
+          helperText={errors.password && errors.password.message}
         />
         {
-          index === 1 ? <TextField 
+          index === 1 ? <><TextField 
           className='formfield'
           label='Confirm Password'
           type='password'
-          value={input.confirmPassword}
-          onChange={(e) => setInput({...input, confirmPassword: e.target.value})}
+          inputRef={register({required: true, validate: (value) => value === getValues('password')})}
+          name='confirmPassword'
+          error={!!errors.confirmPassword}
           variant='filled'
-        /> : null
+          helperText={errors.confirmPassword && "Passwords do not match"}
+        /> 
+        </>
+        : null
         }
         <Button color='primary' type='submit' variant='contained' fullWidth>{index === 0 ? "Login" : "Sign Up"}</Button>
       </form>
-    </Paper>
-  )
+    </Paper> )
 }
