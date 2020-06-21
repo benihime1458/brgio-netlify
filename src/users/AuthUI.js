d36d36b731302fb72dbb775788a3c9ff475e9f6f
baklavacado <benny.l.wu@gmail.com>
2020-06-21 11:59:34 -0700

    rough fire auth login/signup test

--- src/users/AuthUI.js
+++ src/users/AuthUI.js
@@ -1,39 +1,63 @@
 import React, {useState} from 'react'
 import {useForm} from 'react-hook-form'
-import {Paper, TextField, Button, Tabs, Tab} from '@material-ui/core'
+import {Paper, TextField, Button, Tabs, Tab, Typography} from '@material-ui/core'
 import './AuthUI.css'
 import fire from './Fire'
 
 export default AuthUI => {
   const {register, errors, getValues, handleSubmit} = useForm(
     {
-      mode: 'onChange',
-      reValidateMode: 'onChange',
+      mode: 'onSubmit',
+      reValidateMode: 'onBlur',
     }
   )
   const [index, setIndex] = useState(0)
 
-  const submitForm = data => console.log(data)
+  const submitForm = form => {
+    index === 0 ? login(form) : signup(form)
+  }
+  // if index === 0 => Login
+    // fire auth login (form.email, form.password)
+  // if index === 1 => Signup
+    // fire auth sign up (form)
 
-  // const login = e => {
-  //   e.preventDefault()
-  //   fire.auth().signInWithEmailAndPassword(input.email, input.password).then(u => console.log(u)).catch(error => console.log(error))
-  // }
+  const infoCheck = async (query, param) => {
+    const response = await fetch(`/.netlify/functions/getUser?${query}=${param}`)
 
-  // const signup = async e => {
-  //   e.preventDefault()
+    if (query === 'username') {
+      const {data: {username}} = await response.json()
+      return username === param ? "username not available" : true
+    }
+    if (query === 'email') {
+      const {data: {email}} = await response.json()
+      return email === param ? "email not available" : true
+    }
+  }
 
-  //   const res = await fetch('/.netlify/functions/getAllProblems')
-  //   const problems = await res.json()
-  //   console.log('problems: ', problems)
+  const login = ({email, password}) => {
+    // e.preventDefault()
+    fire.auth().signInWithEmailAndPassword(email, password).then(u => console.log(u)).catch(error => console.log(error))
+  }
+
+  const signup = async ({email, password}) => {
+    // e.preventDefault()
+
+    const res = await fetch('/.netlify/functions/getAllProblems')
+    const problems = await res.json()
+    console.log('problems: ', problems)
     
-  //   let newUser = {username: input.username, email: input.email, problemLog: problems}
+    // let newUser = {username: input.username, email: input.email, problemLog: problems}
 
-  //   fire.auth().createUserWithEmailAndPassword(input.email, input.password).then(u => console.log(u)).catch(error => console.log(error.message))
-  // }
+    fire.auth().createUserWithEmailAndPassword(email, password).then(u => console.log(u)).catch(error => console.log(error.message))
+  }
   
   return (
     <Paper className='paper'>
+      <Typography
+        align='center'
+        variant='h1'
+        gutterBottom
+      >brg.io</Typography>
       <Tabs
         className='tabs'
         value={index}
@@ -58,7 +82,7 @@ export default AuthUI => {
           className='formfield'
           label='Desired Username'
           type='text'
-          inputRef={register({required: true, minLength: {value:6, message: 'Must be at least 6 characters'}, maxLength: {value: 20, message: 'Cannot be longer than 20 characters'}, pattern: { value: /^[a-zA-Z0-9]*$/i, message: 'Can only contain alphanumeric characters'}})}
+          inputRef={register({required: true, minLength: {value:6, message: 'Must be at least 6 characters'}, maxLength: {value: 20, message: 'Cannot be longer than 20 characters'}, pattern: { value: /^[a-zA-Z0-9]*$/i, message: 'Can only contain alphanumeric characters'}, validate: (value) => (value.length > 5 && value.length < 21 && index === 1) ? infoCheck('username', value) : true})}
           name='username'
           error={!!errors.username}
           variant='filled'
@@ -70,7 +94,7 @@ export default AuthUI => {
           className='formfield'
           label='Email'
           type='email'
-          inputRef={register({required: true, pattern: {value: /^\S+@\S+$/i, message: 'Invalid email format'}})}
+          inputRef={register({required: true, pattern: {value: /^\S+@\S+$/i, message: 'Invalid email format'}, validate: (value) => (index === 1) ? infoCheck('email', value) : true})}
           name='email'
           error={!!errors.email}
           variant='filled'
