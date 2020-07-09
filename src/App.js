cfd2d6e9eecd9ad0ee6b82b3b18552d20e3ee742
baklavacado <benny.l.wu@gmail.com>
2020-07-09 13:06:28 -0700

    integrate loading and fade in w/setTimeout

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React, { useState, useEffect, useMemo } from 'react';
 import { UserContext } from './context/user-context'
 import AuthUI from './users/AuthUI'
 import Navbar from './components/Navbar'
+import Loading from './components/Loading'
 import ClimbsTable from './components/ClimbsTable';
 import './App.css';
 import fire from './users/Fire';
 
 export default App => {
-  const [user, setUser] = useState(null)
+  const [user, setUser] = useState({loading: false, data: null})
 
   useEffect(() => {
 
@@ -15,18 +16,19 @@ export default App => {
       let email 
       if (user) {
         // User is signed in.
+        setUser({loading: true, data: null})
         email = user.email
-        getUser(email)
+        setTimeout(() => getUser(email), 2000)
       } else {
         // No user is signed in.
-        setUser(null)
+        setUser({loading: false, data: null})
       }
     });
 
     async function getUser(email) {
         const res = await fetch(`/.netlify/functions/users?email=${email}`)
-        const user = await res.json()
-        setUser(user.data)
+        const userByEmail = await res.json()
+        setUser({loading: false, data: userByEmail.data})
     }
 
   }, []);
@@ -37,9 +39,10 @@ export default App => {
     <div className='root'>
       <div className='content'>
         <UserContext.Provider value={UserStore}>
-          <Navbar user={user}/>
-          {!user ? <AuthUI /> : null}
-          {user ? <ClimbsTable problems={user.problemLog} /> : null}
+          <Navbar user={user.data}/>
+          {!user.data && !user.loading ? <AuthUI /> : null}
+          {user.loading ? <Loading /> : null }
+          {!user.loading && user.data ? <ClimbsTable /> : null}
         </UserContext.Provider>
       </div>
     </div>
